144f7baf6af21d0de9abb009e8b64eb15290b4c0
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-08-25 19:03:42 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const path = require('path');
+//  ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// middleware for authentication
+const { authMiddleware } = require('./utils/auth');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+//  a new Apollo server to pass in schema data
+const startServer = async() => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  await server.start();
+
+// integrate Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// wildcard gets route for the server 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
